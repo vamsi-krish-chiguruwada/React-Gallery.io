c1f510b2135d4a1900b73b8887095c769f6cf01f
vamsi-krish-chiguruwada <vamsikrishchiguruwada@gmail.com>
2020-10-21 13:23:04 -0700

    Updates

---
+++ precache-manifest.6f9537adf5ca994c616e9ab8358279b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b4608f3b7bd781d985405050001299",
+    "url": "/React-Gallery.io/index.html"
+  },
+  {
+    "revision": "60bf4171879c09554d45",
+    "url": "/React-Gallery.io/static/css/main.e4088998.chunk.css"
+  },
+  {
+    "revision": "775b9ecba538700ca2b0",
+    "url": "/React-Gallery.io/static/js/2.15271f67.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/React-Gallery.io/static/js/2.15271f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60bf4171879c09554d45",
+    "url": "/React-Gallery.io/static/js/main.4d015400.chunk.js"
+  },
+  {
+    "revision": "120bf530033e378dbbdf",
+    "url": "/React-Gallery.io/static/js/runtime-main.7392ad9f.js"
+  },
+  {
+    "revision": "0e0fd452dc7a7d35c32269c388665363",
+    "url": "/React-Gallery.io/static/media/hero.0e0fd452.jpg"
+  }
+]);
